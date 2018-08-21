ea549b7f7a546f269a78b0e4e08bfe99dc18e6c0
donitechno <35616803+donitechno@users.noreply.github.com>
2018-08-21 10:43:26 +0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -21,7 +21,20 @@ HTML = """
 """
 
 HTML2 = """
-    <h1> travis first</h1>
+<!DOCTYPE html>
+<html lang="en" class="full-height">
+    <head>
+        <title>Home | TravisCI</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="Social media analytic tool">
+        <meta name="author" content="Michal Dyzma">
+    </head>
+    <body>
+        <h1>Home Page 2</h1>
+    </body>
+</html>
 """
 
 #  We have to disale this i pyllint, because pylint will fail our build every
